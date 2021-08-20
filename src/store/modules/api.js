014047abe5e175f9fac4a1048d30941a6681591a
lercc <lercc.en@gmail.com>
2021-08-20 11:32:14 -0500

    little cahnges

--- src/store/modules/api.js
+++ src/store/modules/api.js
@@ -1,7 +1,7 @@
 export default  {
     namespaced: true,
     state: {
-        url: 'http://192.168.1.77:8000'
+        url: 'http://192.168.1.35:8000'
         // url: 'http://127.0.0.1:8000'
         // url: 'https://backend.aupairkids.com.pe'
         // url: 'https://test.aupairkids.com.pe'
